7e1375cc509e981c3cda39e1f04ecc491329a2fa
Eva-004 <foredajannateva@gmail.com>
2026-05-20 23:35:30 +0600

    Create get and post api for comment  field

--- index.js
+++ index.js
@@ -24,45 +24,62 @@ async function run() {
   try {
     await client.connect();
 
-   const db = client.db('ideavault');
-   const ideaCollection = db.collection('ideas');
+    const db = client.db('ideavault');
+    const ideaCollection = db.collection('ideas');
+    const commentCollection = db.collection('comment');
 
-    app.get('/trending-ideas',async(req,res)=>{
-    const result = await ideaCollection.find().limit(6).toArray();
-    res.json(result)
-  })
-   
-    app.post('/all-ideas',async(req,res)=>{
-      const idea=req.body;
-      
-    const result = await ideaCollection.insertOne(idea);
-    res.json(result)
-  })
+    app.get('/trending-ideas', async (req, res) => {
+      const result = await ideaCollection.find().limit(6).toArray();
+      res.json(result)
+    })
 
+    app.post('/all-ideas', async (req, res) => {
+      const idea = req.body;
 
-    app.get('/all-ideas',async(req,res)=>{
-      
+      const result = await ideaCollection.insertOne(idea);
+      res.json(result)
+    })
+
+    app.get('/all-ideas/:id',async (req, res) => {
+      const { id } = req.params;
+      const result = await ideaCollection.findOne({ _id: new ObjectId(id) });
+      res.json(result)
+    })
+
+    app.get('/all-ideas', async (req, res) => {
+
       const search = req.query.search || '';
       const category = req.query.category || '';
-            
-      const query =  {};
-    
-       if(search){
-        
-          query.ideaTitle={
-             $regex: search,
-             $options: "i" 
-          }
-    
-       }
-       if(category){
-        query.category=category
-       }
+
+      const query = {};
+
+      if (search) {
+
+        query.ideaTitle = {
+          $regex: search,
+          $options: "i"
+        }
+
+      }
+      if (category) {
+        query.category = category
+      }
 
       const result = await ideaCollection.find(query).toArray();
-    
-    res.json(result)
-  })
+
+      res.json(result)
+    });
+
+    app.post('/comments', async (req, res) => {
+      const comment = req.body;
+
+      const result = await commentCollection.insertOne(comment);
+      res.json(result)
+    })
+    app.get('/comments', async (req, res) => {
+      const result = await commentCollection.find().toArray();
+      res.json(result)
+    })
 
 
 
@@ -78,11 +95,11 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',async(req,res)=>{
-    res.send('Server is running fine!   ')
+app.get('/', async (req, res) => {
+  res.send('Server is running fine!   ')
 })
 
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 })
